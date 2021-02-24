99a503890c5a391bd4827233416c7aab8d2fcf83
aetos <aetos382@users.noreply.github.com>
2021-02-24 16:49:58 +0900

    実行は大変

--- test/ComparableGenerator.UnitTests/Hoge.cs
+++ test/ComparableGenerator.UnitTests/Hoge.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Loader;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -20,15 +24,19 @@ namespace ComparableGenerator.UnitTests
             string assemblyName = Guid.NewGuid().ToString("D");
             
             var references = new[] {
-                MetadataReference.CreateFromFile(typeof(Type).Assembly.Location)
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
             };
+
+            var options = new CSharpCompilationOptions(
+                OutputKind.DynamicallyLinkedLibrary);
 
             var compilation = CSharpCompilation.Create(
                 assemblyName,
                 new[] {
                     syntaxTree
                 },
-                references);
+                references,
+                options);
 
             return compilation;
         }
@@ -121,6 +129,83 @@ partial class Hoge
             }
 
             Assert.IsEmpty(diagnostics);
+        }
+
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void HogeTest3()
+        {
+            const string source = @"
+using ComparableGenerator;
+
+[Comparable]
+public partial class Hoge
+{
+    [CompareBy(0)]
+    public string FirstName { get; set; }
+
+    [CompareBy(1)]
+    public string LastName { get; set; }
+}";
+
+            var generator = new ComparableGenerator();
+            var driver = CSharpGeneratorDriver.Create(generator);
+            var compilation = CreateCompilation(source);
+
+            driver.RunGeneratorsAndUpdateCompilation(
+                compilation,
+                out var outputCompilation,
+                out var diagnostics);
+
+            foreach (var diag in diagnostics)
+            {
+                TestContext.WriteLine(diag.ToString());
+            }
+
+            Assert.IsEmpty(diagnostics);
+
+            using var assemblyStream = new MemoryStream();
+            using var debugStream = new MemoryStream();
+
+            var emitResult =
+                outputCompilation.Emit(assemblyStream, debugStream);
+
+            foreach (var d in emitResult.Diagnostics)
+            {
+                TestContext.WriteLine(d);
+            }
+
+            if (!emitResult.Success)
+            {
+                Assert.Fail();
+                return;
+            }
+
+            assemblyStream.Seek(0, SeekOrigin.Begin);
+            debugStream.Seek(0, SeekOrigin.Begin);
+
+            var assemblyLoadContext = new TestAssemblyLoadContext();
+            var assembly = assemblyLoadContext.LoadFromStream(
+                assemblyStream, debugStream);
+
+            var type = assembly.GetType("Hoge");
+            var compareTo = type.GetMethod("CompareTo", new[] { type });
+
+            var instance = Activator.CreateInstance(type);
+            var result = (int)compareTo.Invoke(instance, new object[] { instance });
+
+            Assert.AreEqual(0, result);
+
+            assemblyLoadContext.Unload();
+        }
+
+        private class TestAssemblyLoadContext :
+            AssemblyLoadContext
+        {
+            public TestAssemblyLoadContext()
+                : base(true)
+            {
+            }
         }
     }
 }
