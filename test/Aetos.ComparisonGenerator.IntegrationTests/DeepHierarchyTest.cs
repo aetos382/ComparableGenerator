0bb21df2ac00c401dac8b335298270188eb55226
aetos <aetos382@users.noreply.github.com>
2021-03-06 19:23:24 +0900

    add test

---
+++ test/Aetos.ComparisonGenerator.IntegrationTests/DeepHierarchyTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace Aetos.ComparisonGenerator.IntegrationTests
+{
+    public class DeepHierarchyTest :
+        GeneratorTestBase
+    {
+        [Test]
+        public void 深い階層の生成テスト()
+        {
+            const string source = @"
+using Aetos.ComparisonGenerator;
+
+namespace Aetos.ComparisonGenerator
+{
+    namespace IntegrationTest
+    {
+        public partial class Outer
+        {
+            [Comparable]
+            public partial class Inner
+            {
+                [CompareBy]
+                public int Value { get; set; }
+            }
+        }
+    }
+}";
+
+            var generator = new ComparableObjectGenerator();
+
+            var assembly = RunGeneratorAndGenerateAssembly(
+                generator,
+                source,
+                out _);
+
+            Assert.That(assembly, Is.Not.Null);
+
+            var type = assembly.GetType("Aetos.ComparisonGenerator.IntegrationTest.Outer+Inner");
+
+            Assert.That(type, Is.Not.Null);
+        }
+    }
+}
